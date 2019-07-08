c3c7cd058c7881bb1c41d9839356f423041e5f33
Simon Anderson <simon.anderson111@gmail.com>
2019-07-08 11:24:53 +1000

    Landing page made, Controller commented out

--- peapi/src/main/java/com/peapi/peapi/PeapiRootController.java
+++ peapi/src/main/java/com/peapi/peapi/PeapiRootController.java
@@ -1,15 +1,14 @@
 package com.peapi.peapi;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+
+//@RestController
 public class PeapiRootController {
 
-    @RequestMapping
-    public String home(){
-        return "Generic root page";
-    }
+    //@GetMapping("/")
+    //public String rootGet(){
+    //    return "Generic root page";
+    //}
     
 
 }
