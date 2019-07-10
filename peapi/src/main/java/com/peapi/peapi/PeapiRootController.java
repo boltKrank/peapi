88ec836977aa555929c0c61a0ead721c0c6d0c08
Simon Anderson <simon.anderson111@gmail.com>
2019-07-10 14:04:53 +1000

    Add extra controllers

--- peapi/src/main/java/com/peapi/peapi/PeapiRootController.java
+++ peapi/src/main/java/com/peapi/peapi/PeapiRootController.java
@@ -1,14 +1,120 @@
 package com.peapi.peapi;
 
-
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 //@RestController
 public class PeapiRootController {
+
+    //root (/)
+    @GetMapping("/")
+    public String rootGet(){
+
+        return "rootGet";
+
+    }
+    
+    @PostMapping("/")
+    public String rootPost(){
+
+        return "rootPost";
+
+    }
+    
+    //rbac1
+    @GetMapping("/rbac1")
+    public String rbac1Get(){
+
+        return "rbac1Get";
+    
+    }
+    
+    @PostMapping("/rbac1")
+    public String rbac1Post(){
+
+        return "rbac1Post";
+    
+    }
+    
+
+    //rbac2
+    @GetMapping("/rbac2")
+    public String rbac2Get(){
+
+        return "rbac2Get";
+    
+    }
+    
+    @PostMapping("/rbac1")
+    public String rbac2Post(){
+
+        return "rbac2Post";
+    
+    }
+
+    //node
+    @GetMapping("/node")
+    public String nodeGet(){
+
+        return "nodeGet";
+    
+    }
+
+    @PostMapping("/rbac1")
+    public String nodePost(){
+
+        return "nodePost";
+    
+    }
+
+    //orch
+    @GetMapping("/orch")
+    public String orchGet(){
+
+        return "orchGet";
+
+    }
+    
+    @PostMapping("/orch")
+    public String orchPost(){
+
+        return "orchPost";
+
+    }
+ 
+
+    //code
+    @GetMapping("/code")
+    public String codeGet(){
+
+        return "codeGet";
+
+    }
+    
+    @PostMapping("/code")
+    public String codePost(){
+
+        return "codePost";
+
+    }
+ 
+
+    //activity
+    //razor
+    //admin
+    //server
+    //server_status
+    //http
+    //ca
+    //db
+    //forge
+
+
+
 
     //@GetMapping("/")
     //public String rootGet(){
     //    return "Generic root page";
     //}
-    
-
+ 
 }
