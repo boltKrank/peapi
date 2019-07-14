160ba4b5bb651fd07ab88d8d5c330990591b4609
Simon Anderson <simon.anderson111@gmail.com>
2019-07-15 09:51:08 +1000

    Daily update (broken)

--- peapi/src/main/java/com/peapi/peapi/PeapiRootController.java
+++ peapi/src/main/java/com/peapi/peapi/PeapiRootController.java
@@ -2,8 +2,9 @@ package com.peapi.peapi;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-//@RestController
+@RestController
 public class PeapiRootController {
 
     //root (/)
